01a1bededae93d9764366dece66e9bfa3ac0e09e
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-11-30 17:15:21 +0000

    corrected dns/delete-all-a-records-for-each-server.sh

--- dns/delete-all-a-records-for-each-server.sh
+++ dns/delete-all-a-records-for-each-server.sh
@@ -8,13 +8,15 @@ API_HOST=https://api.cloudns.net
 API_PATH=/dns/delete-record.json?
 API_AUTH=auth-id="$CLOUDNS_AUTH_ID&auth-password=$CLOUDNS_AUTH_PASSWORD&"
 
-for SERVER_HOSTNAME in $(cat ./servers.txt)
+for SERVER_IP in $(cat ./servers.txt)
 do
-  SERVER_IP=$(dig +short $SERVER_HOSTNAME)
-  SERVER_IP=$(echo -n $SERVER_IP)
-  A_RECORD_ID=$(./dns/get-all-a-records.sh | jq -r --arg SERVER_IP "$SERVER_IP" 'select(.record | contains($SERVER_IP)) | {id} | join("")')
-  echo $A_RECORD_ID
-  echo "About to delete A record for $SERVER_IP"
-  curl "$API_HOST$API_PATH$API_AUTH&domain-name=$DOMAIN&record-id=$A_RECORD_ID"
+  A_RECORD_IDS=$(./dns/get-all-a-records.sh | jq -r --arg SERVER_IP "$SERVER_IP" 'select(.record | contains($SERVER_IP)) | {id} | join("")')
+
+  for A_RECORD_ID in $A_RECORD_IDS
+  do
+    echo "About to delete A record for $SERVER_IP"
+    curl "$API_HOST$API_PATH$API_AUTH&domain-name=$DOMAIN&record-id=$A_RECORD_ID"
+  done
 done
+
 exit 0
