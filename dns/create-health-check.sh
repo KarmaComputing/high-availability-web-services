cb2cbeef68580e05f1a81254f5a1bb7f2990275c
Joel <joel@karmacomputing.co.uk>
2021-11-30 20:29:25 -0400

    creating dns monitoring automation script

--- dns/create-health-check.sh
+++ dns/create-health-check.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
+##FAILOVER PARAMETERS
+##https://www.cloudns.net/wiki/article/274/
+
 set -x
-export $(xargs < ../.env)
+export $(xargs < .env)
 
 # Create all wildcard records for each server
 API_HOST=https://api.cloudns.net
 API_PATH=/dns/failover-activate.json?
 API_AUTH="auth-id=$CLOUDNS_AUTH_ID&auth-password=$CLOUDNS_AUTH_PASSWORD&"
 echo "$API_AUTH"
-# Get all server IPs
 
-SERVER_PUBLIC_IPS=$(../hetzner/hetzner-get-all-servers-ip-public-net.sh)
-SERVER_RECORDS_ID=$(./get-all-a-records-id.sh)
-for SERVER_PUBLIC_IP in $SERVER_PUBLIC_IPS 
-  for SERVER_RECORDS_ID in $SERVER_RECORDS_ID
-  do
-  do
+# Get all dns records IDs and IPs
+RECORD_ID_AND_IP=$(./dns/get-all-wildcards.sh | jq -r '. | {id,record} | join (",")') 
+
+for RECORD_ID_AND_IP in $RECORD_ID_AND_IP
+do
+  echo "$RECORD_ID_AND_IP"
+  RECORD_ID=$(echo $RECORD_ID_AND_IP | cut -d "," -f 1)
+  RECORD_IP=$(echo $RECORD_ID_AND_IP | cut -d "," -f 2)
+
     echo "FAILOVER"
     echo $API_HOST$API_PATH$API_AUTH
-    curl "$API_HOST$API_PATH$API_AUTH&domain-name=$DOMAIN&type=a&record-id=$MISSING&check_type=8&host=*&record=$SERVER_PUBLIC_IP&port=443&down_event_handler=1&up_event_handler=1&main_ip=$SERVER_PUBLIC_IP&check_period=60"
-
-  done  
+    curl "$API_HOST$API_PATH$API_AUTH&domain-name=$DOMAIN&record-id=$RECORD_ID&check_type=8&host=*&record=$RECORD_IP&port=443&down_event_handler=1&up_event_handler=1&main_ip=$RECORD_IP&check_period=60&notification_mail=-1"
   done
-  exit 0
-
+ exit 0
 
