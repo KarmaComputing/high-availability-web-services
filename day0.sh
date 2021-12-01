59beaa72a892b2d7538433d35b60fa608e140ff1
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-01 15:53:55 +0000

    added ./run during setup

--- day0.sh
+++ day0.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+rm -rf ./run
+# Copy over/create dirs
+find . -type d -not -path './*git*' -not -path './*run*' -print -exec mkdir -p './run/{}' \;
+# Copy over/create files into dirs
+find . -type f -not -path './*git*' -not -path './*run*' -print -exec cp -a '{}' 'run/{}' \;
+
+# Change to run directory
+cd run
+pwd
+
+./rename-domain.sh example.co.uk $1
+
 ./hetzner/hetzner-create-n-servers.sh 3
 cp servers.txt servers.bk
 ./hetzner/hetzner-get-all-servers-ip-public-net.sh > servers.txt
