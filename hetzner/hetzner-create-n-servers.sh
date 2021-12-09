1a2e7182192f7038e0342562f3b0ba0779118d74
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-09 22:15:22 +0000

    allow to set type of hetzner server during server creation as argument 2

--- hetzner/hetzner-create-n-servers.sh
+++ hetzner/hetzner-create-n-servers.sh
@@ -3,9 +3,20 @@
 set -x
 export $(xargs <.env)
 
-# Create n hetzner servers
+# Usage: ./hetzner/hetzner-create-n-servers.sh 3 cx11
+# Note: Server type must be in lowercase
 
+# Create n hetzner servers
 NUMBER_OF_SERVERS=$1
+SERVER_TYPE=$2
+
+if [[ $# -ne 2 ]]
+then
+  echo WARNING: Using default server type cx11
+  SERVER_TYPE=cx11
+fi
+
+
 DATACENTER=nbg1-dc3
 
 
@@ -17,8 +28,8 @@ do
     -X POST \
     -H "Authorization: Bearer $HETZNER_API_TOKEN" \
     -H "Content-Type: application/json" \
-    -d '{"automount":false,"datacenter":"nbg1-dc3","firewalls":[],"image":"ubuntu-20.04","labels":{}, "name":"'$SERVER_NAME'","networks":[],"server_type":"cx11","ssh_keys":["chris@chris-ideapad","joel@ubuntu"],"start_after_create":true,"user_data":"","volumes":[]}' \
-    'https://api.hetzner.cloud/v1/servers'
+    -d '{"automount":false,"datacenter":"nbg1-dc3","firewalls":[],"image":"ubuntu-20.04","labels":{}, "name":"'$SERVER_NAME'","networks":[],"server_type":"'$SERVER_TYPE'","ssh_keys":["chris@chris-ideapad","joel@ubuntu"],"start_after_create":true,"user_data":"","volumes":[]}' \
+    'https://api.hetzner.cloud/v1/servers' > new-server.json
 done
 
 echo $INDEX servers created
