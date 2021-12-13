4d2328219e55e2a5df5548f137a59c57e709001c
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-13 21:29:56 +0000

    #4 add tidb/generate-tidb-topology.sh

---
+++ tidb/generate-tidb-topology.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Generates TiDB topology yaml
+# See https://docs.pingcap.com/tidb/v5.3/hybrid-deployment-topology
+
+DB_SERVERS=$(cat ../db-servers.txt)
+
+function list_first_server {
+  FIRST_SERVER=$(sed -n 1p ../db-servers.txt)
+echo "  - host: $FIRST_SERVER"
+}
+
+function list_servers {
+for DB_SERVER in $DB_SERVERS
+do
+echo "  - host: $DB_SERVER"
+done
+}
+
+echo "global:"
+echo "  user: 'tidb'"
+echo "  ssh_port: 22"
+echo "  deploy_dir: '/mnt/data1/tidb-deploy'"
+echo "  data_dir: '/mnt/data1/tidb-data'"
+echo "server_configs: {}"
+echo "pd_servers:"
+list_servers
+echo
+echo "tidb_servers:"
+list_servers
+echo "tikv_servers:"
+list_servers
+echo "monitoring_servers:"
+list_first_server
+echo "grafana_servers:"
+list_first_server
+echo "alertmanager_servers:"
+list_first_server
