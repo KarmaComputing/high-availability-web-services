574ae76ca122d1ba3ec5d347b492fdf4772578d8
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-12-13 21:31:22 +0000

    #4 servers.txt -> db-servers.txt

--- provision-database.sh
+++ provision-database.sh
@@ -2,7 +2,7 @@
 set -x
 
 ./hetzner/hetzner-create-n-servers.sh 3 cpx11
-./hetzner/hetzner-get-all-servers-ip-public-net.sh > servers.txt
+./hetzner/hetzner-get-all-servers-ip-public-net.sh > db-servers.txt
 
 sleep 5
 
@@ -16,13 +16,13 @@ echo Attach one of each volume to each of the servers
 # Generate keys to bootstrap Tidb cluster
 ssh-keygen -f id_rsa -N "" <<< y
 
-for SERVER in $(cat servers.txt)
+for SERVER in $(cat db-servers.txt)
 do
   ssh root@$SERVER apt update
   echo Copy key for bootstraping Tidb cluster
   scp id_rsa* root@$SERVER:/root/.ssh
   ssh root@$SERVER tee -a .ssh/authorized_keys < id_rsa.pub
-  for TARGET in $(cat servers.txt)
+  for TARGET in $(cat db-servers.txt)
   do
     ssh root@$TARGET -C "ssh-keyscan -H $TARGET >> ~/.ssh/known_hosts"
   done
@@ -50,12 +50,13 @@ do
 done
 sleep 35
 # Install TiUP on first node
-scp topology.yaml root@$(sed -n 1p servers.txt):~
+.tidb/generate-tidb-topology.sh > topology.yaml
+scp topology.yaml root@$(sed -n 1p db-servers.txt):~
 ssh root@$(sed -n 1p servers.txt) -C "curl --proto '=https' --tlsv1.2 -sSf https://tiup-mirrors.pingcap.com/install.sh | sh"
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster"
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup update --self && tiup update cluster"
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup --binary cluster"
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster check ./topology.yaml"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup update --self && tiup update cluster"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup --binary cluster"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster check ./topology.yaml"
 RETURN_CODE=$?
 if [ $RETURN_CODE -ne 0 ]
 then
@@ -63,15 +64,16 @@ then
   exit 255
 fi
 # Deploy TiDB cluster
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster deploy tidb-database v5.3.0 ./topology.yaml <<< y" #auto confirm yes
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster deploy tidb-database v5.3.0 ./topology.yaml <<< y" #auto confirm yes
 # Start the TiDB cluster
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster start tidb-database"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster start tidb-database"
 # List the clusters (we only created one)
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster list"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster list"
 # Show status of cluster
-ssh root@$(sed -n 1p servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster display tidb-database"
+ssh root@$(sed -n 1p db-servers.txt) -C "export PATH=/root/.tiup/bin:$PATH && tiup cluster display tidb-database"
 # Install mysql-client (for testing only)
-ssh root@$(sed -n 1p servers.txt) -C "apt-get install -y mysql-client"
+ssh root@$(sed -n 1p db-servers.txt) -C "apt-get install -y mysql-client"
 # Verify mysql protocol and Tidb
-ssh root@$(sed -n 1p servers.txt) -C "mysql -h 127.0.0.1 -P 4000 -e 'SELECT NOW()'"
+ssh root@$(sed -n 1p db-servers.txt) -C "mysql -h 127.0.0.1 -P 4000 -e 'SELECT NOW()'"
 # Set password: 
+echo Important: Set password now. Connect to cluster and issue e.g. SET PASSWORD='secret'
